865d2013cd6e6f2168932766ff627d3da68052ad
DANIIL <gusarov_ib18@st.ithub.ru>
2019-05-14 17:04:28 +0300

    1

--- Reference App/Controller/DetailViewController.swift
+++ Reference App/Controller/DetailViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class DetailViewController: UITableViewController {
     
-    @IBOutlet weak var imageView: UIImageView!
+//    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var costLabel: UILabel!
     
     var model: ReferenceModel!
     
@@ -22,8 +23,9 @@ class DetailViewController: UITableViewController {
     }
     
     func updateUI() {
-        imageView.image = model.image
+//        imageView.image = model.image
         titleLabel.text = model.title
         detailLabel.text = model.detail
+        costLabel.text = "\(Date())"
     }
 }
